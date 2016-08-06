5ec0ed1ebc355cad5fb8ef4294ee76d229b5fbc7
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-06 15:11:31 -0600

    resize image

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE-edge">
+		<meta name="viewport" content="width-device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<!--<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (required for Bootstap's JS plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	</head>
+	<body class="sfooter-content">
+		<div class="sfooter-content">
+			<!--Header and Navigation bar-->
+			<header>
+				<div class="container">
+					<!--Bootstrap Navigation Bar-->
+					<nav class="navbar navbar-inverse">
+						<!-- logo and mobile toggle button get grouped together for better mobile display -->
+						<div class="navbar-header">
+							<!-- this is the mobile menu button -->
+							<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+								<span class="sr-only">main menu</span>
+								<span class="glyphicon glyphicon-menu-hamburger"></span>
+							</button>
+							<a class="navbar-brand" href="#">Diane's Web Page</a>
+						</div>
+
+						<!-- here are your main nav links, grouped for toggling -->
+						<div class="collapse navbar-collapse" id="main-menu">
+							<a class="navbar-icon"> </a>
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#"></a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Contact</a></li>
+								<li><a href="#">Blog</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
+			<main>
+				<div class="container">
+					<div class="row">
+					<div class="col-md-12">
+						<div class="jumbotron">
+							<h1>Hello !</h1>
+				</div>
+							<!--begin 2 column row-->
+							<div class="row">
+								<div class="col-md-6">
+									<div class="content-box">
+										<h3>Photos</h3>
+
+										<img src="images/burton.jpg" class="img-fluid" alt="tim-burton">
+
+									</div>
+								</div>
+								<div class="row">
+								<div class="col-md-6">
+									<div class="content-box">
+										<h3>Flexbox Sticky Footer</h3>
+										<p>Keep the footer at the bottom of the page, always. Even if the content-area is "short". This is the finest sticky footer I've ever used. View the source code.</p>
+									</div>
+								</div>
+							</div>
+							<h1>hello!</h1>
+
+						</div>
+			</main>
+
+
+
+
+		</div>
+	</body>
+</html>
+
+
